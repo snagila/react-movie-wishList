15419088bc135a1ca28fb30ea1bc3b0fbea0f7c6
Sachin Nagila <sachinnagila2053@gmail.com>
2024-06-11 22:39:34 +1000

    merged

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+const MovieDetails = ({ searchedMovies }) => {
+  return (
+    <>
+      <Col style={{ height: "40vh" }} className="movieDetails">
+        <div className="ms-5">
+          <Row>
+            <h3 className="mt-3 ">{searchedMovies.Plot}</h3>
+          </Row>
+          <Row className="ms-1 ">Actors: {searchedMovies.Actors}</Row>
+        </div>
+      </Col>
+    </>
+  );
+};
+
+export default MovieDetails;
